1050cb9d149f1fd95a3e778976272ce7ab0629eb
Kamalusta <kamalusta@gmail.com>
2023-10-14 11:52:49 -0700

    seventh

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = sizeof(int);
+int c = sizeof(char);
+int li = sizeof(long int);
+int lli = sizeof(long long int);
+int f = sizeof(float);
+printf("Size of a char: %d byte(s)\n", c);
+printf("Size of a int: %d byte(s)\n", i);
+printf("Size of a long int: %d byte(s)\n", li);
+printf("Size of a long long int: %d byte(s)\n", lli);
+printf("Size of a float: %d byte(s)\n", f);
+
+return (0);
+}
