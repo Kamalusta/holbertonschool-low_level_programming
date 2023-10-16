8df9107bcffaebfa3bbe5ed361c993dfd1cf8627
Kamalusta <kamalusta@gmail.com>
2023-10-16 11:07:40 -0700

    third

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+int leng = strlen(str);
+ 
+for(i=0;i<leng;i++)
+{
+char c = str[i];
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
