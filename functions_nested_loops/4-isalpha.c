bf24c35e8b62c8f715fe3d7e7cf2a39fc118b46d
Kamalusta <kamalusta@gmail.com>
2023-10-17 22:14:43 -0700

    fifth

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - check the code gor lower case
+ *@c: The char to be cheked
+ *
+ * Return: Always 1 is succes.
+ */
+int _isalpha(int c)
+{
+if ((c > 64 && c < 91) || (c > 96 && c < 123))
+return (1);
+else
+return (0);
+}
