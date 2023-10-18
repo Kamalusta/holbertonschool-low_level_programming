ab2eed503eba5b8462e6baef6f9808feccce8c90
Kamalusta <kamalusta@gmail.com>
2023-10-17 21:46:25 -0700

    fourth

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _islower - check the code.
+ * _islower - check the code gor lower case
  *
- * Return: Always 0.
+ * Return: Always 1 is succes.
  */
 int _islower(int c)
 {
-if (c < 96 && c < 123)
+if (c > 96 && c < 123)
 return (1);
 else
 return (0);
