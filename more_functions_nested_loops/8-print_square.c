231d2879bdb436acf23ee705a55a69c4450bd4cc
Kamalusta <kamalusta@gmail.com>
2023-10-21 11:56:44 -0700

    ninth

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - check the code
+ *@size: siz of square
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+  int s = size;
+  int c = size;
+  if (size < 1)
+    _putchar('\n');
+  while (s > 0)
+    {
+    while (c > 0)
+      {
+	 _putchar('#');
+	 c--;
+      }
+    _putchar('\n');
+    s--;
+    c = size;
+    }
+
+}
