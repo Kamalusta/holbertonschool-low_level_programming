668a701f237f9ec721f96db118b247498fabcc2e
Kamalusta <kamalusta@gmail.com>
2023-10-21 11:59:38 -0700

    ninth

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,20 +7,20 @@
  */
 void print_square(int size)
 {
-  int s = size;
-  int c = size;
-  if (size < 1)
-    _putchar('\n');
-  while (s > 0)
-    {
-    while (c > 0)
-      {
-	 _putchar('#');
-	 c--;
-      }
-    _putchar('\n');
-    s--;
-    c = size;
-    }
+int s = size;
+int c = size;
 
+if (size < 1)
+_putchar('\n');
+while (s > 0)
+{
+while (c > 0)
+{
+_putchar('#');
+c--;
+}
+_putchar('\n');
+s--;
+c = size;
+}
 }
