6cfe07b34746083e526d476c5d175f90b6490012
Kamalusta <kamalusta@gmail.com>
2023-10-21 09:26:11 -0700

    seventh

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+      while (n > 0)
+	{
+	  _putchar(95);
+	  n--;
+	}
+      _putchar('\n');
+}
