91333d012d1f80b9cec5818fa129761f2f450903
Kamalusta <kamalusta@gmail.com>
2023-10-22 12:21:01 -0700

    twelfth

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -9,18 +9,16 @@ int main(void)
 {
 long int n = 612852475143;
 int r;
-int l;
 int i;
-for(i = 2;i < n; i++)
+for (i = 2; i < n; i++)
 {
 r = n % i;
 if (r == 0)
 {
 n = n / i;
-l = i;
 i--;
 }
 }
-printf("%d\n", l);
+printf("%ld\n", n);
 return (0);
 }
