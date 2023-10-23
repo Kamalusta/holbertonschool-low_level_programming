ead10050a8a01be080c9d78df25dabe3f86a6e39
Kamalusta <kamalusta@gmail.com>
2023-10-23 01:15:49 -0700

    thirteenth

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -7,30 +7,21 @@
  */
 void print_number(int n)
 {
-int m;
-int a;
+int m = n;
+int a = n;
 int c = 1;
 int k = 0;
-if (n < 0)
-{
-m = n * (-1);
-a = n * (-1);
-}
-else
-{
-m = n;
-a = n;
-}
+
 if (n < 0)
 _putchar('-');
 if (n == 0)
 _putchar('0');
 else
 {
-while (a > 0)
+while (a != 0)
 {
 a = a / 10;
-if (a > 0)
+if (a != 0)
 c = c * 10;
 else if (c < 10)
 _putchar('0' + m);
@@ -38,11 +29,17 @@ _putchar('0' + m);
 while (c > 1)
 {
 k = m / c;
+if (n < 0)
+k = k * (-1);
 _putchar('0' + k);
 m = m % c;
 c = c / 10;
-if (m < 10 && c == 1)
+if (m < 10 && m > -10 && c == 1)
+  {
+    if (n < 0)
+m = m * (-1);
 _putchar('0' + m);
+  }
 }
 }
 }
