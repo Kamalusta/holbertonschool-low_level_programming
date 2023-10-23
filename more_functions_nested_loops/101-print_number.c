9bc46d9c254e692c372fa7929e238996a2dc6b2d
Kamalusta <kamalusta@gmail.com>
2023-10-23 00:15:48 -0700

    thirteenth

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_number - check the code
+ *@n: given number
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+int m;
+int a;
+int c = 1;
+int k = 0;
+if (n < 0)
+{
+m = n * (-1);
+a = n * (-1);
+}
+else
+{
+m = n;
+a = n;
+}
+if (n == 0)
+_putchar('0');
+else
+{
+while (a > 0)
+{
+a = a / 10;
+if (a > 0)
+c = c * 10;
+}
+while (c > 1)
+{
+k = m / c;
+if (n < 0)
+_putchar('-');
+_putchar('0' + k);
+m = m % c;
+c = c / 10;
+if (m < 10 && c == 1)
+_putchar('0' + m);
+}
+}
+}
