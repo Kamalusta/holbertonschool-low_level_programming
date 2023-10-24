bb7aacb6643aa3fa8ca38a6deac9f0f0348b33a5
Kamalusta <kamalusta@gmail.com>
2023-10-24 09:57:14 -0700

    eighth

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ *@str: pointer
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+int len = 0;
+int i;
+while (str[len] > 0)
+len++;
+for (i = len / 2; i < len; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
