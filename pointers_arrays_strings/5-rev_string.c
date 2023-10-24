142ac56c5f0279e1396de9bc6a0cec7639354138
Kamalusta <kamalusta@gmail.com>
2023-10-24 07:07:09 -0700

    sixth

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code
+ *@s: pointer
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+int i = 0;
+int len;
+while (s[i] > 0)
+{
+i++;
+len = i - 1;
+}
+for (i = 0; len / 2 > i; i++)
+{
+int tmp = s[i];
+s[i] = s[len - i];
+s[len - i] = tmp;
+}
+}
