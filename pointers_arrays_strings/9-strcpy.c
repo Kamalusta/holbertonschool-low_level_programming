464d399bcff1f2b6b032f3e43d31b9c53acc0fcc
Kamalusta <kamalusta@gmail.com>
2023-10-25 10:19:08 -0700

    tenth

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-  int i;
-  while (src[i] >= 0)
-    {
-      dest[i] = src[i];
+  int i = 0;
+  int j = 0;
+  while (src[i] > 0)
       i++;
+  for (j = 0; j < i; j++)
+    {
+      dest[j] = src[j];
     }
+    
   return dest;
 }
