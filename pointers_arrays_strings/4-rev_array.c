665e8ccc488cde1969a9fd6a4f546186cf2b9212
Kamalusta <kamalusta@gmail.com>
2023-10-26 11:11:47 -0700

    fifth

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+int i = 0;
+for (i = 0; i < n / 2; i++)
+{
+int temp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
+}
+}
