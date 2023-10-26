458b7d1d29d5e51f0c9b74f51e87ebc4d9d61d6a
Kamalusta <kamalusta@gmail.com>
2023-10-26 04:53:19 -0700

    second

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -13,12 +13,21 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
 int j = 0;
+ int k = 0;
+ int z;
 while (dest[i])
 i++;
-while (j < n)
+ while (src[k])
+   k++;
+ if (k >= n)
+   z = n;
+ else
+   z = k;
+   
+for (j = 0; j < z; j++)
 {
 dest[i + j] = src[j];
-j++;
 }
+   
 return (dest);
 }
