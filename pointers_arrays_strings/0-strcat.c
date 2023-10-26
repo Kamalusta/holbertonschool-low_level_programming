4b8a8fd992a85c83f2429516e087585582c055bc
Kamalusta <kamalusta@gmail.com>
2023-10-26 02:39:18 -0700

    first

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - check the code
+ *@dest first string
+ *@src second string
+ *
+ * Return: Always pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+while (dest[i])
+i++;
+while (src[j])
+{
+dest[i+j] = src[j];
+j++;
+} 
+return (dest);
+}
