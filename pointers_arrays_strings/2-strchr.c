0d0c8929a75bc9b3d054078c6c8ac0a3b1126df9
Kamalusta <kamalusta@gmail.com>
2023-10-27 11:05:01 -0700

    third

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - check the code
+ * @s: -char point
+ * @c: - char
+ *
+ * Return: Always char pointer
+ */
+char *_strchr(char *s, char c)
+{
+int i, j;
+int k = 0;
+static char arr[90];
+for (i = 0; s[i]; i++)
+{
+if (s[i] == c && k == 0)
+{
+k = 1;
+for (j = 0; s[i + j]; j++)
+arr[j] = s[i + j];
+}
+}
+if (k == 1)
+return (arr);
+else
+return (NULL);
+}
