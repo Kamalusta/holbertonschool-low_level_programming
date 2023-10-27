0fdcca6f1e032c0b7dbe7c8896276d062e8daf29
Kamalusta <kamalusta@gmail.com>
2023-10-27 12:05:35 -0700

    fourth

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - check the code
+ * @s: first array
+ * @V: second one
+ *
+ * Return: Always int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+unsigned int k = 0;
+for (i = 0; s[i] && i == k; i++)
+{
+for (j = 0; accept[j]; j++)
+{
+if (s[i] == accept[j])
+k++;
+}
+}
+return (k);
+}
