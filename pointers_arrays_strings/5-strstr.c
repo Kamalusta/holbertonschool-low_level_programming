79aeefa5c78ce7569b70627d6304cea3b4c6bb26
Kamalusta <kamalusta@gmail.com>
2023-10-28 02:41:04 -0700

    sixth

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - check the code
+ * @haystack: pointer
+ * @needle; pointer 2
+ *
+ * Return: Always pinter to char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0;
+int j;
+for (j = 0; haystack[j]; j++)
+{
+if (haystack[j] == needle[i])
+i++;
+else
+i = 0;
+if (needle[i] == '\0')
+break;
+}
+if (needle[i] == '\0')
+return (haystack + (j - i));
+else
+return (0);
+}
