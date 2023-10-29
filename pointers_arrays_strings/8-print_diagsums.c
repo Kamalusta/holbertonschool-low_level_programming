d26bd6fc79d67a6c90c0f90c5a8dbddeb621b183
Kamalusta <kamalusta@gmail.com>
2023-10-29 00:18:17 -0700

    last

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -16,6 +16,6 @@ int d2 = 0;
 for (i = 0; i < size * size; i += (size + 1))
 d1 += a[i];
 for (j = size - 1; j < size * size - (size - 1); j += (size - 1))
-d2 += aj];
-printf("%d, %d\n", d1, d2)
+d2 += a[j];
+printf("%d, %d\n", d1, d2);
 }
