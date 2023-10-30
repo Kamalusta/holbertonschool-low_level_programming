3b2420793bf8b2364851d9485c694d1e99ea5a38
Kamalusta <kamalusta@gmail.com>
2023-10-30 01:24:15 -0700

    first

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ * @s: pointer char
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+if (!s[0])
+_putchar('\n');
+else
+{
+_putchar(s[0]);
+_puts_recursion(s + 1);
+}
+}
