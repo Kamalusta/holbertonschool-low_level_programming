5fe8feefdc31fcc9bb5bf2046c99dd2511f5b013
Kamalusta <kamalusta@gmail.com>
2023-11-02 07:42:07 -0700

    last

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,21 +13,24 @@
 
 int main(int argc, char *argv[])
 {
+int j;
 int i;
 int res = 0;
 if (argc < 2)
 printf("0\n");
 for (i = 1; i < argc; i++)
 {
-if (atoi(argv[i]) > 0)
-res += atoi(argv[i]);
-else
+for (j = 0; argv[i][j]; j++)
+{
+if (argv[i][j] < 48 || argv[i][j] > 57)
 {
 printf("Error\n");
 res = 0;
 return (1);
 break;
 }
+}
+res += atoi(argv[i]);
 }
 if (res)
 printf("%d\n", res);
