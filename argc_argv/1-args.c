1f7674e90edd8525b868537905347b5985763998
Kamalusta <kamalusta@gmail.com>
2023-11-02 00:18:48 -0700

    second

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - give the count arguments
+ * @argv: it is void
+ * @argc: all atributies
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
