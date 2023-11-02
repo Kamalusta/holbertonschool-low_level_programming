5200936b09408b0b59a71f39cbd4ccdb9a718462
Kamalusta <kamalusta@gmail.com>
2023-11-02 00:12:58 -0700

    first

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * main - give the name program
+ * @argc: it is void
+ * @argv: all atributies
+ *
+ * Return Always 0. 
+ */
 
-int main (int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-  printf("%s \n", argv[0]);
-  return (0);
+printf("%s\n", argv[0]);
+return (0);
 }
