b233665ad9444bd5dcd1d3d78d7dc0a5d305ad0e
Kamalusta <kamalusta@gmail.com>
2023-11-07 04:20:58 -0800

    first

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 char *arr = malloc(size);
 unsigned int i = 0;
 if (arr == NULL || size == 0)
-  return (NULL);
+return (NULL);
 else
 {
 while (i < size)
