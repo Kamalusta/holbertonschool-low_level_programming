90d8aa60be747480953d97caa7211c49d0f29c63
Kamalusta <kamalusta@gmail.com>
2023-11-13 01:37:21 -0800

    second

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - check the code
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+int i = 0;
+int j = 0;
+char *cpy;
+if (str == NULL)
+return (NULL);
+else
+{
+while (str[i])
+i++;
+cpy = malloc(sizeof(char) * i);
+while (i)
+{
+cpy[j] = str[j];
+i--;
+j++;
+}
+return (cpy);
+}
+}
