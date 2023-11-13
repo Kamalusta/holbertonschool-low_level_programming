c118ac49367df8e645c28574c3216d72921b5c25
Kamalusta <kamalusta@gmail.com>
2023-11-13 09:31:15 -0800

    correction

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,22 +11,22 @@
  */
 int **alloc_grid(int width, int height)
 {
-int i = 0;
-int j;
+int i;
 int **arr2d;
 if (width <= 0 || height <= 0)
 return (NULL);
-else
-{
 arr2d = malloc(sizeof(int *) * width);
 if (arr2d == NULL)
 return (NULL);
-else
 for (i = 0; i < width; i++)
+{
 *(arr2d + i) = malloc(sizeof(int) * height);
-for (i = 0; i < width; i++)
-for (j = 0; j < height; j++)
-arr2d[i][j] = 0;
-return (arr2d);
+if (*(arr2d + i) == NULL)
+{
+return (NULL);
+free(*(arr2d + i));
+free(arr2d);
 }
+}
+return (arr2d);
 }
