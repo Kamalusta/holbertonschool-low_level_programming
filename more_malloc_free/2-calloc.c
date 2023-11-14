e844215111df8969f909864576a23b61c9adab33
Kamalusta <kamalusta@gmail.com>
2023-11-14 05:48:44 -0800

    third

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - prints buffer in hexa
+ * @nmemb: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *arr;
+if (nmemb == 0 || size == 0)
+return (NULL);
+arr = malloc(nmemb * size);
+if (!arr)
+return (NULL);
+for (i = 0; i < nmemb; i++)
+arr[i] = 0;
+return (arr);
+}
