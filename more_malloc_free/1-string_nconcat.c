c1ae7369f278bbf4152a0f9a4517ff211948e7e2
Kamalusta <kamalusta@gmail.com>
2023-11-14 04:52:48 -0800

    correction

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int i = 0;
 unsigned int j = 0;
 char *arr;
+if (!s1)
+s1 = "";
+if (!s2)
+s2 = "";
 while (s1[i])
 i++;
 while (s2[j])
@@ -28,6 +32,6 @@ for (j = 0; j < i; j++)
 arr[j] = s1[j];
 for (j = 0; j < n; j++)
 arr[i + j] = s2[j];
-arr[i + j + 1] = 0;
+/*arr[i + j + 1] = 0;*/
 return (arr);
 }
