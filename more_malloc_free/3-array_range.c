ae1aa7737c299dabc4a0f95fc4fca326c4b06292
Kamalusta <kamalusta@gmail.com>
2023-11-14 10:42:17 -0800

    fourth

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,8 +14,10 @@ int *array_range(int min, int max)
 {
 int *arr;
 int i = 0;
+if (min > max)
+return (NULL);
 arr = malloc(sizeof(int) * (max - min + 1));
-if (min > max || !arr)
+if (!arr)
 return (NULL);
 while (min <= max)
 {
