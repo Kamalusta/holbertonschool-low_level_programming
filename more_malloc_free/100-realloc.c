20e66d985eef93985281069c1eac8db769619414
Kamalusta <kamalusta@gmail.com>
2023-11-18 01:34:28 -0800

    correction

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -13,24 +13,24 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-  if (ptr == NULL)
-    {
-      ptr = malloc(new_size);
-      return (ptr);
-    }
-  else if (new_size == 0 && ptr != NULL)
-    {
-      free(ptr);
-      return (NULL);
-    }
-  else if (new_size == old_size)
-    return (ptr);
-  else if (new_size > old_size)
-    {
-      free(ptr);
-      ptr = malloc(old_size + new_size);
-      return (ptr);
-    }
-  else
-    return (NULL);
+if (ptr == NULL)
+{
+ptr = malloc(new_size);
+return (ptr);
+}
+else if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+else if (new_size == old_size)
+return (ptr);
+else if (new_size > old_size)
+{
+free(ptr);
+ptr = malloc(old_size + new_size);
+return (ptr);
+}
+else
+return (NULL);
 }
