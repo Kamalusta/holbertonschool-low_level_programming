dc3db0776d6cebc50838a4cad205ae0e450cce48
Kamalusta <kamalusta@gmail.com>
2023-11-20 05:36:39 -0800

    second

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - check the code
+ * @d: struct
+ *
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+if (!d)
+return;
+else
+{
+if (!d->name)
+d->name = "(nil)";
+printf("Name: %s\n", d->name);
+if (!d->age)
+printf("Age: (nil)");
+else
+printf("Age: %f\n", d->age);
+if (!d->owner)
+d->owner = "(nil)";
+printf("Owner: %s\n", d->owner);
+}
+}
