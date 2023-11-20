4174ad4d5d732bf7ffe626ef813c04ed62de518c
Kamalusta <kamalusta@gmail.com>
2023-11-20 09:40:09 -0800

    correction

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,12 +12,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+int i = 0;
+int j;
+char *name2, *owner2;
 dog_t *dog;
+while (*(name + i))
+{
+i++;
+}
+name2 = malloc(i + 1);
+for (j = 0; j < i; j++)
+name2[j] = name[j];
+i = 0;
+while (owner[i])
+i++;
+owner2 = malloc(i + 1);
+for (j = 0; j < i; j++)
+owner2[j] = owner[j];
 dog = malloc(sizeof(dog_t));
 if (!dog)
 return (NULL);
-dog->name = name;
+dog->name = name2;
 dog->age = age;
-dog->owner = owner;
+dog->owner = owner2;
 return (dog);
+free(dog);
+free(name2);
+free(owner2);
 }
