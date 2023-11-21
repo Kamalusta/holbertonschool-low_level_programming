3278185be81524f7b2aa0664f4af5520e54502d2
Kamalusta <kamalusta@gmail.com>
2023-11-21 01:02:36 -0800

    second

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer
+ * @array: the array to print
+ * @size: size elements
+ * @action: function
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned long int i;
+if (!action)
+return;
+for (i = 0; i < size; i++)
+action(array[i]);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,4 +2,5 @@
 #define _FUNCTION_POINTER_H
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* _FUNCTION_POINTER_H */
