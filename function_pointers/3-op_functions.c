6a29ff439bbf76fd7a616d0e1087aa9024a4ab97
Kamalusta <kamalusta@gmail.com>
2023-11-22 05:33:31 -0800

    opr

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - function
+ * @a: first integer 
+ * @b: second integer
+ *
+ */
+int op_add(int a, int b)
+{
+  return (a + b);
+}
+
+/**
+ * op_sub - function
+ * @a: first integer
+ * @b: second integer
+ *
+ */
+int op_sub(int a, int b)
+{
+  return (a - b);
+}
+
+/**
+ * op_mul - function
+ * @a: first integer
+ * @b: second integer
+ *
+ */
+int op_mul(int a, int b)
+{
+  return (a * b);
+}
+
+/**
+ * op_div - function
+ * @a: first integer
+ * @b: second integer
+ *
+ */
+int op_div(int a, int b)
+{
+  return (a / b);
+}
+
+/**
+ * op_mod - function
+ * @a: first integer
+ * @b: second integer
+ *
+ */
+int op_mod(int a, int b)
+{
+  return (a % b);
+}
