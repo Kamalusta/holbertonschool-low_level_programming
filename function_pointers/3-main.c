c3cedfd29cedca6c1eec4ccebbba2061a5c94014
Kamalusta <kamalusta@gmail.com>
2023-11-22 05:32:18 -0800

    last

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int res;
+int (*op)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit (98);
+}
+if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+{
+printf("Error\n");
+exit (100);
+}
+op = get_op_func(argv[2]);
+if (!op)
+{
+printf("Error\n");
+exit (99);
+}
+res = op(atoi(argv[1]), atoi(argv[3]));
+printf("%d\n", res);
+return (0);
+}
