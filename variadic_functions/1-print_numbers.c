c1782f53d6a968cb03bd42f58988423575fad0aa
Kamalusta <kamalusta@gmail.com>
2023-11-23 02:17:27 -0800

    second

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,11 +13,11 @@ va_list ls;
 unsigned int i;
 va_start(ls, n);
 for (i = 0; i < n; i++)
-  {
-    printf("%d", va_arg(ls, int));
- if (separator && i < n-1)
-   printf("%s", separator);
-     }
- va_end(ls);
- printf("\n");
+{
+printf("%d", va_arg(ls, int));
+if (separator && i < n - 1)
+printf("%s", separator);
+}
+va_end(ls);
+printf("\n");
 }
