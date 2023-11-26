979dddbd0e7db4fed176962c657d6d6e74a7691b
Kamalusta <kamalusta@gmail.com>
2023-11-26 11:56:01 -0800

    last

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,7 +2,19 @@
 #define _VARIADIC_FUNCTION_H
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+/**
+ * struct print - print
+ * @type: The operator
+ * @f: The function associated
+ */
+typedef struct print
+{
+char *type;
+void (*f)(va_list ap);
+} print_type;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif /* _VARIADIC_FUNCTION_H */
