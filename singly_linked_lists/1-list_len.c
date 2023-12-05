5721917afc1986b72a0f7e6cc8d862629a87a205
Kamalusta <kamalusta@gmail.com>
2023-12-04 20:54:52 -0800

    second

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - check the code
+ * @h: - node;
+ *
+ * Return: Always 0.
+ */
+size_t list_len(const list_t *h)
+{
+size_t i = 0;
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i);
+}
