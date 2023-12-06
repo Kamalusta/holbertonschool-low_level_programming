e9c5077c5e3c71dc7a8a373334abf4e33509bf19
Kamalusta <kamalusta@gmail.com>
2023-12-06 07:03:22 -0800

    seventh

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - check the code
+ * @head: - head node
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+if (!head)
+return (NULL);
+while (head->next)
+{
+head = head->next;
+sum += head->n;
+}
+return (sum);
+}
