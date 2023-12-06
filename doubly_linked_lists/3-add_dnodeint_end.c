f5048f566ea4e15ff71a5f8db3d5b35ede228a07
Kamalusta <kamalusta@gmail.com>
2023-12-06 04:27:08 -0800

    fourth

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - check the code
+ * @head: - head node
+ * @n: - number
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *newnode = malloc(sizeof(dlistint_t));
+dlistint_t *last;
+if (!newnode)
+return (NULL);
+newnode->n = n;
+newnode->next = NULL;
+if (!(*head))
+*head = newnode;
+else
+while (last->next)
+last = last->next;
+last->next = newnode;
+newnode->prev = last;
+return (newnode);
+}
