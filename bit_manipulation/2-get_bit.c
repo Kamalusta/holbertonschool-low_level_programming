17999f5dfc8c492d0604bcb2280b3fb6267247eb
Kamalusta <kamalusta@gmail.com>
2023-12-09 08:51:02 -0800

    correction

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,6 +9,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+unsigned long int i = 0, m = n;
+while (m)
+{
+m >>= 1;
+i++;
+}
+if (index >= i)
+return (-1);
 n = n >> index;
 if (n & 1)
 return (1);
