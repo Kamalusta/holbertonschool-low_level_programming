affa9b7e8ccbf57496369502cd7d01a6baf0f10c
Kamalusta <kamalusta@gmail.com>
2023-12-10 10:52:24 -0800

    advanced

---
+++ bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - Checks the endianness
+ *
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+unsigned int n = 1;
+char *i = (char *)&n;
+if (*i == 1)
+return (1);
+}
+else
+return (0);
+}
