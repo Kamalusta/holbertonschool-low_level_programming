c2f6691ebc821dfaeca01711f4b6e6377dbc2cab
Kamalusta <kamalusta@gmail.com>
2023-12-12 03:12:35 -0800

    first

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * read_textfile - check the code
+ * @filename: - name of file
+ * @letters: - number of written letters
+ *
+ * Return: number of read.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buffer;
+int fd, rd, wr;
+buffer = malloc(letters);
+if (!buffer)
+return (0);
+if (!filename)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+return (0);
+rd = read(fd, buffer, letters);
+if (rd < 0)
+return (0);
+wr = write(STDIN_FILENO, buffer, letters);
+if (wr < 0 || wr != (int)letters)
+return (0);
+close(fd);
+return (rd);
+}
