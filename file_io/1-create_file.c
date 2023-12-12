872b8456972c6394d6ff8f024fd4e075290af68f
Kamalusta <kamalusta@gmail.com>
2023-12-12 11:04:15 -0800

    second

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ if (!filename)
 return (-1);
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (text_content)
-wr = write(fd, text_content, sizeof(text_content));
+wr = write(fd, text_content, strlen(text_content));
 if (fd == -1 || wr == -1)
 return (-1);
 return (1);
--- file_io/main.h
+++ file_io/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
