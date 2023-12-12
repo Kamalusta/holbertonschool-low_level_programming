c7656e10d319289d53a1f26a861057fa14a0e7e9
Kamalusta <kamalusta@gmail.com>
2023-12-12 08:33:17 -0800

    correction

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buffer;
 int fd, rd, wr;
-buffer = malloc(letters);
+buffer = malloc(letters + 1);
 if (!buffer)
 return (0);
 if (!filename)
@@ -22,9 +22,11 @@ return (0);
 rd = read(fd, buffer, letters);
 if (rd < 0)
 return (0);
+buffer[rd] = '\0';
 wr = write(STDIN_FILENO, buffer, letters);
 if (wr < 0 || wr != (int)letters)
 return (0);
 close(fd);
+free(buffer);
 return (rd);
 }
