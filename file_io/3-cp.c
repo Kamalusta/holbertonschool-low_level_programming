ef904a9bdd3eee9095b8728a6cec854ac1261d1d
Kamalusta <kamalusta@gmail.com>
2023-12-12 13:15:35 -0800

    fourth

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,7 +14,7 @@ char buf[1024];
 if (ac != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 fd = open(av[1], O_RDONLY);
-if (f1 < 0)
+if (fd < 0)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 exit(98);
