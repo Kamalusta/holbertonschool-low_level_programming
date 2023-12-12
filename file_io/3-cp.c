c98c9f35188a10e63dfb52ce68176e65ee4b4986
Kamalusta <kamalusta@gmail.com>
2023-12-12 12:56:22 -0800

    fourth

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,6 +5,7 @@
  * @ac: - nbr argument
  * @av: - arguments
  *
+ * Return: always 0.
  */
 int main(int ac, char **av)
 {
@@ -13,26 +14,26 @@ char *buf = malloc(strlen(av[1]));
 if (ac != 3)
 {
 exit(97);
-dprintf(1,"Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 }
 fd = open(av[1], O_RDONLY);
-rd = read(fd, buf, 1024);
+rd = read(fd, buf, strlen(av[1]));
 if (!av[1] || fd == -1 || rd == -1)
 {
 exit(98);
-dprintf(1, "Error: Can't read from file %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 }
 fd1 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-wr = write(fd1, buf, 1024);
+wr = write(fd1, buf, strlen(av[1]));
 if (fd1 == -1 || wr == -1)
 {
 exit(99);
-dprintf(1, "Error: Can't write to  %s\n", av[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", av[2]);
 }
 if (close(fd) == -1)
 {
 exit(100);
-dprintf(1, "Error: Can't close fd %d\n", fd);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 }
 return (0);
 }
