7965735e0ca7c4fd5ecf7972c61a36eb7f2fbba5
Kamalusta <kamalusta@gmail.com>
2023-12-14 10:55:54 -0800

    free

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,31 +11,30 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int mykey;
-hash_node_t *new, *col = malloc(sizeof(hash_node_t));
-if (!ht || !key || !col)
+hash_node_t *new, *col;
+if (!ht || !key)
 return (0);
 mykey = key_index((unsigned char *)key, ht->size);
 if (ht->array[mykey] != 0)
 {
-if (!strcmp(ht->array[mykey]->key, key))
+col = ht->array[mykey];
+while (col)
 {
-free(ht->array[mykey]->value);
-ht->array[mykey]->value = strdup(value);
-return (1);
-}
-col->key = strdup(key);
+if (!strcmp(col->key, key))
+{
+free(col->value);
 col->value = strdup(value);
-col->next = ht->array[mykey];
-ht->array[mykey] = col;
 return (1);
 }
-free(col);
+col = col->next;
+}
+}
 new = malloc(sizeof(hash_node_t));
 if (!new)
 return (0);
 new->key = strdup(key);
 new->value = strdup(value);
-new->next = NULL;
+new->next = ht->array[mykey];
 ht->array[mykey] = new;
 return (1);
 }
